0019064aeca9fa0654825ce9f388554a002e6592
Jonathan Swartz <swartz@pobox.com>
2009-07-18 03:42:29 +0000

    Import of JSWARTZ/Log-Any-Adapter-Dispatch-0.03 from CPAN.
    gitpan-cpan-distribution: Log-Any-Adapter-Dispatch
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JSWARTZ/Log-Any-Adapter-Dispatch-0.03.tar.gz
    gitpan-cpan-author:       JSWARTZ
    gitpan-cpan-maturity:     released

---
+++ lib/Log/Any/Adapter/Dispatch.pm
@@ -0,0 +1,70 @@
+package Log::Any::Adapter::Dispatch;
+use Carp qw(croak);
+use Log::Any::Util qw(make_method);
+use strict;
+use warnings;
+use base qw(Log::Any::Adapter::Base);
+
+our $VERSION = '0.03';
+
+sub init {
+    my ($self) = @_;
+
+    croak 'must supply dispatcher' unless defined( $self->{dispatcher} );
+}
+
+# Delegate logging methods to same methods in dispatcher
+#
+foreach my $method ( Log::Any->logging_methods() ) {
+    __PACKAGE__->delegate_method_to_slot( 'dispatcher', $method, $method );
+}
+
+# Delegate detection methods to would_log
+#
+foreach my $method ( Log::Any->detection_methods() ) {
+    my $level = substr( $method, 3 );
+    make_method( $method,
+        sub { my ($self) = @_; return $self->{dispatcher}->would_log($level) }
+    );
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Log::Any::Adapter::Dispatch
+
+=head1 SYNOPSIS
+
+    use Log::Dispatch;
+    my $dispatcher = Log::Dispatch->new();
+    $dispatcher->add(Log::Dispatch::File->new(...));
+    $dispatcher->add(Log::Dispatch::Screen->new(...));
+    Log::Any->set_adapter('Dispatch', dispatcher => $dispatcher);
+
+=head1 DESCRIPTION
+
+This Log::Any adapter uses L<Log::Dispatch|Log::Dispatch> for logging. There is
+a single required parameter, I<dispatcher>, which must be a valid Log::Dispatch
+object.
+
+=head1 SEE ALSO
+
+L<Log::Any|Log::Any>, L<Log::Dispatch|Log::Dispatch>
+
+=head1 AUTHOR
+
+Jonathan Swartz
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2007 Jonathan Swartz, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'Log::Any::Adapter::Dispatch' );
+}
+
+diag( "Testing Log::Any::Adapter::Dispatch $Log::Any::Adapter::Dispatch::VERSION, Perl $], $^X" );
